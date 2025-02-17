2337f4f9c4761879f2c45b35a6fe43e0e669fcbf
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-02-17 16:22:28 +0500

    completed task

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-const colorBtn = document.getElementById("colorBtn");
+// Select the button for applying input color
+const applyColorBtn = document.getElementById("applyColorBtn");
+
+// Select the button for applying input color
+const randomColorBtn = document.getElementById("colorBtn");
+
+// Select the button for stopping color change
+const stopColorBtn = document.getElementById("stopColorBtn");
+
+// Select the input field
+const colorInput = document.getElementById("colorInput");
+
+const titleText = document.getElementById("textChange");
 
 // Select the body for background change
 const body = document.body;
 
 let digits = "01234567789ABCDEF";
 let color = "#";
+let intervalID;
+
+const getColorValue = () => {
+  let userColor = colorInput.value; // Get the value from input field
+  console.log(userColor);
+  body.style.backgroundColor = userColor;
+  titleText.textContent = `Current Color: ${userColor}`;
+  // return userColor;
+};
 
 hexColorCodeGenerator = () => {
   for (let i = 0; i < 6; i++) color += digits[Math.floor(Math.random() * 16)];
-  console.log(color);
+  console.log(color, "generated color");
+
   body.style.backgroundColor = color;
-  color= '#'
+  titleText.textContent = `Current Color: ${color}`;
+  color = "#";
+  console.log(color, "color");
 };
 
 colorBtn.addEventListener("click", () => {
   setTimeout(() => {
-    setInterval(() => {
+    intervalID = setInterval(() => {
       hexColorCodeGenerator();
     }, 1000);
   }, 2000);
 });
+
+stopColorBtn.addEventListener("click", () => {
+  clearInterval(intervalID);
+  console.log(intervalID); // Stop the interval
+  intervalID = null; // Reset interval ID
+  console.log("Color change stopped.");
+});
+
+applyColorBtn.addEventListener("click", () => getColorValue());
